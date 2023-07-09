16878b30fee950b62c29bb9e17075e70549df48e
Luis Capuyon <frostehhh967@gmail.com>
2023-07-09 20:45:45 +0800

    Install simple-import-sort eslint plugin

--- packages/web/.eslintrc.cjs
+++ packages/web/.eslintrc.cjs
@@ -17,9 +17,11 @@ module.exports = {
   parserOptions: {
     project: path.join(__dirname, 'tsconfig.json'),
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
