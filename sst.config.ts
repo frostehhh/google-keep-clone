f4b4ee1ff4d764764bb0a78dda5ee1f416f804c2
Luis Capuyon <frostehhh967@gmail.com>
2023-07-10 21:19:46 +0800

    Reuse eslint-config-custom in root dir

--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,12 @@
-import { SSTConfig } from "sst";
-import { Default } from "./stacks/Default";
+import { type SSTConfig } from 'sst';
+
+import { Default } from './stacks/Default';
 
 export default {
   config(_input) {
     return {
-      name: "google-keep-clone",
-      region: "us-east-1",
+      name: 'google-keep-clone',
+      region: 'us-east-1',
     };
   },
   stacks(app) {
--- stacks/Default.ts
+++ stacks/Default.ts
@@ -1,8 +1,8 @@
-import { StackContext, NextjsSite } from "sst/constructs";
+import { NextjsSite,type StackContext } from 'sst/constructs';
 
 export function Default({ stack }: StackContext) {
-  const site = new NextjsSite(stack, "site", {
-    path: "packages/web",
+  const site = new NextjsSite(stack, 'site', {
+    path: 'packages/web',
   });
   stack.addOutputs({
     SiteUrl: site.url,
