9e545188fc566f3aa6057931460fd23c13fcd3bb
Luis Capuyon <frostehhh967@gmail.com>
2023-07-11 21:59:00 +0800

    Add eslint config to functions

---
+++ packages/functions/.eslintrc.cjs
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+module.exports = {
+  overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: path.join(__dirname, 'tsconfig.json'),
+      },
+    },
+  ],
+  parserOptions: {
+    project: path.join(__dirname, 'tsconfig.json'),
+  },
+  extends: ['custom'],
+};
