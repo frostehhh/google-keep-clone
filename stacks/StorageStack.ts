8fccb0d20e458dc2e6551d1bf01595196346d448
Luis Capuyon <frostehhh967@gmail.com>
2023-07-23 06:52:58 +0800

    Update StorageStack to reuse DynamoDB info obj

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,12 +1,17 @@
-import { type StackContext,Table } from 'sst/constructs';
+import { DynamoDBInfo } from '@google-keep-clone/core';
+import { type StackContext, Table } from 'sst/constructs';
 
 export function StorageStack({ stack }: StackContext) {
-  const notesTable = new Table(stack, 'Notes', {
+  const noteEntity = DynamoDBInfo.Notes.Entity.Note;
+  const partitionKey = noteEntity.partitionKey.toString() as keyof typeof noteEntity.attributes;
+  const sortKey = noteEntity.sortKey?.toString() as keyof typeof noteEntity.attributes;
+
+  const notesTable = new Table(stack, DynamoDBInfo.Notes.tableName, {
     fields: {
-      userId: 'string',
-      noteId: 'string',
+      [partitionKey]: noteEntity.attributes[partitionKey].type,
+      [sortKey]: noteEntity.attributes[sortKey].type,
     },
-    primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+    primaryIndex: { partitionKey, sortKey },
   });
 
   return {
