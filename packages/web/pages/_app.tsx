63ed5095fcb4e35296b117e58e140d7d24709697
Luis Capuyon <frostehhh967@gmail.com>
2023-07-24 03:34:04 +0800

    Revert "Setup Mantine"
    This reverts commit 5651ec44004aa1dc17d616df2d9fe9a363c79897.

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,26 +1,7 @@
 import '@/styles/globals.css';
 
-import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-        /** Put your mantine theme override here */
-          colorScheme: 'light',
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- packages/web/pages/_document.tsx
+++ packages/web/pages/_document.tsx
@@ -1,19 +1,13 @@
-import { createStylesServer, ServerStyles } from '@mantine/next';
-import Document, { type DocumentContext } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-// optional: you can provide your cache as a first argument in createStylesServer function
-const stylesServer = createStylesServer();
-
-export default class _Document extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles html={initialProps.html} server={stylesServer} key="styles" />,
-      ],
-    };
-  }
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
