072d9539610ec2aa780e3d05e45c51afc3dcc377
Luis Capuyon <frostehhh967@gmail.com>
2023-07-27 02:56:12 +0800

    Support const TableName type in TableInfo class

--- packages/core/src/dynamodb/classes/TableInfo/TableInfo.ts
+++ packages/core/src/dynamodb/classes/TableInfo/TableInfo.ts
@@ -1,10 +1,10 @@
 import { type AttributeDefinitions, type TableInfoConstructor } from './types';
 
-export class TableInfo<ReadOnlyAttributeDefinitions extends Readonly<AttributeDefinitions> = Readonly<AttributeDefinitions>> {
-  public tableName: string;
+export class TableInfo<const TableName, ReadOnlyAttributeDefinitions extends Readonly<AttributeDefinitions> = Readonly<AttributeDefinitions>> {
+  public tableName: TableName;
   public attributes: ReadOnlyAttributeDefinitions;
 
-  constructor({ tableName, attributes }: TableInfoConstructor<ReadOnlyAttributeDefinitions>) {
+  constructor({ tableName, attributes }: TableInfoConstructor<TableName, ReadOnlyAttributeDefinitions>) {
     this.tableName = tableName;
     this.attributes = attributes;
   }
--- packages/core/src/dynamodb/classes/TableInfo/types.ts
+++ packages/core/src/dynamodb/classes/TableInfo/types.ts
@@ -1,6 +1,6 @@
 export type AttributeDefinitions = Record<string | number | symbol, unknown>;
 
-export type TableInfoConstructor<ReadOnlyAttributeDefinitions extends Readonly<AttributeDefinitions> = Readonly<AttributeDefinitions>> = {
-  tableName: string;
+export type TableInfoConstructor<TableName, ReadOnlyAttributeDefinitions extends Readonly<AttributeDefinitions> = Readonly<AttributeDefinitions>> = {
+  tableName: TableName;
   attributes: ReadOnlyAttributeDefinitions;
 }
