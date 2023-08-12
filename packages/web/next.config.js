b5314c59f286fe7bb70a33284935d6c3e7df027b
Luis Capuyon <frostehhh967@gmail.com>
2023-08-12 15:48:54 +0800

    Ignore aws-crt warning
    References:
    - https://github.com/aws-amplify/amplify-js/issues/11030#issuecomment-1598207365

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // Avoid AWS SDK Node.js require issue
+    if (isServer && nextRuntime === 'nodejs')
+      config.plugins.push(
+        new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+      );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
