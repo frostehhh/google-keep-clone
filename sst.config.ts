e134a118d4cecb507deff1ffc8564b0118a0f477
Luis Capuyon <frostehhh967@gmail.com>
2023-09-17 03:37:28 +0800

    Remove envSchema usage from root
    Remove envSchema usage from root since this is unnecessarily
    invoked when running web project.

--- sst.config.ts
+++ sst.config.ts
@@ -1,12 +1,9 @@
-import { EnvSchema } from '@google-keep-clone/core';
 import { type SSTConfig } from 'sst';
 
 import { ApiStack } from './stacks/ApiStack';
 import { AuthStack } from './stacks/AuthStack';
 import { FrontendStack } from './stacks/FrontendStack';
 import { StorageStack } from './stacks/StorageStack';
-
-EnvSchema.parse(process.env);
 
 export default {
   config(_input) {
