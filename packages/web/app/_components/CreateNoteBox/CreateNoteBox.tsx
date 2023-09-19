bbddb947337247b15d3a64ad496a5c16d72120de
Luis Capuyon <frostehhh967@gmail.com>
2023-09-20 01:04:44 +0800

    Migrate CreateNoteBox to use react-hook-form

--- packages/web/app/_components/CreateNoteBox/CreateNoteBox.tsx
+++ packages/web/app/_components/CreateNoteBox/CreateNoteBox.tsx
@@ -1,4 +1,6 @@
-import { useForm } from '@tanstack/react-form';
+'use client';
+
+import { useController, useForm } from 'react-hook-form';
 
 import TextArea from '@/components/ui/TextArea';
 import TextField from '@/components/ui/TextField';
@@ -6,50 +8,29 @@ import TextField from '@/components/ui/TextField';
 import { useFocusWithinCreateNote } from './CreateNotebox.utils';
 
 const CreateNoteBox = () => {
-  const form = useForm({
+  const { watch, formState, reset, control } = useForm({
     defaultValues: {
       title: '',
       content: '',
     },
+    mode: 'onChange',
   });
-  const { focused, focusWithinProps } = useFocusWithinCreateNote({ values: form.state.values, isFormTouched: form.state.isTouched, resetForm: form.reset });
+  const { focused, focusWithinProps } = useFocusWithinCreateNote({ values: watch(), isFormTouched: formState.isDirty, resetForm: reset });
+  const { field: titleInputProps } = useController({ name: 'title', control });
+  const { field: contentInputProps } = useController({ name: 'content', control });
 
   return (
     <div {...focusWithinProps} className="w-96 h-50 shadow-lg shadow-indigo-500/40">
-      <form.Provider>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            void form.handleSubmit();
-          }}
-        >
-          {focused && (
-            <div>
-              <form.Field name="title">
-                {(field) => {
-                  return <TextField name={field.name} value={field.state.value} placeholder='Title' onChange={(value) => field.handleChange(value)} />;
-                }}
-              </form.Field>
-            </div>
-          )}
-          <div>
-            <form.Field name="content">
-              {(field) => {
-                return (
-                  <TextArea
-                    name={field.name}
-                    value={field.state.value}
-                    onChange={(value) => field.handleChange(value)}
-                    classes={{ root: 'px-4 py-3 box-content h-5	text-sm', textarea: 'h-full w-full outline-none' }}
-                    placeholder="Take a note..."
-                    aria-label='Take a note...' />
-                );
-              }}
-            </form.Field>
-          </div>
-        </form>
-      </form.Provider>
+      <form>
+        {focused && (
+          <TextField {...titleInputProps} placeholder='Title' />
+        )}
+        <TextArea
+          {...contentInputProps}
+          classes={{ root: 'px-4 py-3 box-content h-5	text-sm', textarea: 'h-full w-full outline-none' }}
+          placeholder="Take a note..."
+          aria-label='Take a note...' />
+      </form>
 
     </div>
   );
