24b5187b27e61cc3a3700ded2983e5cf3d43b037
Luis Capuyon <frostehhh967@gmail.com>
2023-09-24 19:19:45 +0800

    Workaround for deployment issue for Cognito
    [Issue]
    
    When running the `sst deploy` script, the SST framework temporarily
    sets the `userPoolId` to a value like `${TOKEN[TOKEN***]}`, which is not
    valid according to `aws-amplify` configure method.
    
    [Workaround]
    
    We temporarily set the id to `temp_poolId` so `aws-amplify` will not
    throw an error, and to fix the deployment.

--- packages/web/api/awsConfig.ts
+++ packages/web/api/awsConfig.ts
@@ -1,3 +1,9 @@
+
+let userPoolId = process.env.NEXT_PUBLIC_USER_POOL_ID!;
+if (!/^[\w-]+_[0-9a-zA-Z]+$/.test(userPoolId)) {
+  userPoolId = 'temp_poolId';
+}
+
 export const config = {
   API: {
     endpoints: [
@@ -11,7 +17,8 @@ export const config = {
   Auth: {
     mandatorySignIn: true,
     region: process.env.NEXT_PUBLIC_REGION,
-    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    // userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolId,
     identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID,
     userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
   },
